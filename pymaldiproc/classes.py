87f9c7871d02cc70c640e9a819756120c30fe3e7
unknown <gtluu912@gmail.com>
2022-07-24 22:06:32 -0700

    add basic dash app with graph

--- pymaldiproc/classes.py
+++ pymaldiproc/classes.py
@@ -6,10 +6,12 @@ from pyteomics import mzml as pyt
 class MALDISpectrum(object):
     def __init__(self, pyteomics_dict):
         self.id = None
-        self.replicate = None
+        self.replicate = 1
         self.chip = None
         self.spot = None
         self.ms_level = None
+        self.mz_array = None
+        self.mz_array = None
         self.raw_mz_array = None
         self.raw_intensity_array = None
         self.preprocessed_mz_array = None
@@ -22,12 +24,19 @@ class MALDISpectrum(object):
 
     def parse_pyteomics_dict(self, pyteomics_dict):
         # metadata
-        spectrum_id = pyteomics_dict['spectrum title'].split(' ')[0]
-        spectrum_filename = pyteomics_dict['spectrum title'].split(' ')[1]
-        self.id = spectrum_id.split('.')[0]
-        self.replicate = spectrum_id.split('.')[1]
-        self.chip = spectrum_filename.split('\\')[-4].split('_')[0]
-        self.spot = spectrum_filename.split('\\')[-4].split('_')[1]
+        if 'spectrum title' in pyteomics_dict.keys():
+            spectrum_id = pyteomics_dict['spectrum title'].split(' ')[0]
+            spectrum_filename = pyteomics_dict['spectrum title'].split(' ')[1]
+            self.id = spectrum_id.split('.')[0]
+            self.replicate = spectrum_id.split('.')[1]
+            self.chip = spectrum_filename.split('\\')[-4].split('_')[0]
+            self.spot = spectrum_filename.split('\\')[-4].split('_')[1]
+        # placeholder else clause
+        # TODO: update TIMSCONVERT to include spectrum title
+        # TODO: add counter as id number to pyteomics_dict in MALDIDataset.import_mzml()
+        else:
+            self.id = '1'
+            self.replicate = '1'
 
         # spectra
         self.ms_level = pyteomics_dict['ms level']
@@ -46,10 +55,14 @@ class ConsensusPeakList(object):
 
 class MALDIDataset(object):
     def __init__(self, input_path):
-        self.spectra = []
+        #self.spectra = []
+        self.spectra = {}
         self.consensus_peak_lists = []
 
         self.import_mzml(input_path)
+        self.spectra_ids = [spectrum_id + '|' + spectrum_replicate
+                            for spectrum_id, value in self.spectra.items()
+                            for spectrum_replicate, value in self.spectra[spectrum_id].items()]
 
     def import_mzml(self, input_path):
         # find mzML files
@@ -63,7 +76,9 @@ class MALDIDataset(object):
         for mzml_filename in input_files:
             mzml_data = list(pyt.read(mzml_filename))
             for scan_dict in mzml_data:
-                self.spectra.append(MALDISpectrum(scan_dict))
+                spectrum = MALDISpectrum(scan_dict)
+                #self.spectra.append(spectrum)
+                self.spectra[spectrum.id] = {spectrum.replicate: spectrum}
 
     #
     def generate_consensus_peak_lists(self, method):
