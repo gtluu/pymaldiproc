66aa61d327057d0e8b91e2bcbe0665ebf3aa7db8
gtluu <gtluu912@gmail.com>
2024-04-03 19:00:52 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 from setuptools import setup
+import os
 from pymaldiproc import VERSION
+
+
+if os.path.isfile('requirements.txt'):
+    with open('requirements.txt', 'r') as requirements_file:
+        install_requires = requirements_file.read().splitlines()
+for package in install_requires:
+    if package.startswith('git'):
+        pname = package.split('/')[-1].split('.')[0]
+        install_requires[install_requires.index(package)] = pname + ' @ ' + package
 
 setup(name='pyMALDIproc',
       version=VERSION,
-      packages=['pymaldiproc', 'pymaldiviz'],
       url='https://github.com/gtluu/pyMALDIproc',
       license='Apache License',
       author='Gordon T. Luu',
       author_email='gtluu912@gmail.com',
-      description='pyMALDIproc: a Python package for working with MALDI mass spectrometry data')
+      packages=['pymaldiproc', 'pymaldiviz', 'etc'],
+      include_package_data=True,
+      package_data={'': ['*.cfg']},
+      description='pyMALDIproc: a Python package for working with MALDI mass spectrometry data',
+      install_requires=install_requires,
+      setup_requires=install_requires)
